c2677aaafa05ee2255f609819e72d7c7c0c3cf81
emmanuel-ebiwari <ebiwariemma@gmail.com>
2024-11-07 18:26:39 +0100

    configure jest for testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true, // Enable SWC minification
+    reactStrictMode: true,
+    swcMinify: true, // Enable SWC minification
 };
 
 export default nextConfig;
